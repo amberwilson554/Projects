6318ef1890288b59e90f40e173f7ef047dfe798e
amberwilson554 <amber.wilson554@gmail.com>
2021-11-28 20:13:41 -0500

    Add files via upload

---
+++ Cone_HourGlass/Cone_HourGlass/Art.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+public class Art
+{
+	public void ConeArt(int numofstars)
+    {
+		string spaces = " ";
+
+		for(int i = numofstars; i != 0; i--)
+        {
+			spaces += " ";
+			Console.Write(spaces);
+				for (int j = 0; j < numofstars; j++)
+				{
+				    Console.Write("");
+				    Console.Write("*");    
+				}
+			Console.WriteLine("");
+			numofstars = numofstars - 2;
+			
+			if(numofstars < 0)
+            {
+				break;
+            }
+		}
+    }
+
+
+	public void HourglassArt(int totalnumstars)
+    {
+		string spaces = " ";
+		int checkodd = 0;
+		int numofstarsbottom = 0;
+		int numofstarstop = totalnumstars;
+	
+		while (numofstarstop > 0)
+		{
+			Console.WriteLine("");
+			spaces += " ";
+			Console.Write(spaces);
+
+				for (int j = 0; j < numofstarstop; j++)
+				{
+					Console.Write("*");
+				}
+
+				if(numofstarstop >= 2)
+				{
+					numofstarstop = numofstarstop - 2;
+				}
+				else
+				{
+					numofstarsbottom = numofstarstop;
+					numofstarstop = 0;
+				}
+		}
+		checkodd = totalnumstars % 2;
+		while (numofstarsbottom <= totalnumstars)
+		{
+
+			if (checkodd != 0)
+			{
+				Console.WriteLine("");
+				Console.Write(spaces);
+
+				spaces += " ";
+				for (int k = 1; k <= numofstarsbottom; k++)
+				{
+					Console.Write("*");
+				}
+				if (spaces.Length > 1)
+				{
+					spaces = spaces.Remove(0, 2);
+				}
+				numofstarsbottom = numofstarsbottom + 2;
+			}
+			else
+            {
+				spaces += " ";
+				Console.Write(spaces);
+				
+				for (int k = 1; k <= numofstarsbottom; k++)
+				{
+					Console.Write("*");
+				}
+				if (spaces.Length > 1)
+				{
+					Console.WriteLine("");
+					spaces = spaces.Remove(0, 2);
+				}
+				numofstarsbottom = numofstarsbottom + 2;
+			}
+
+		}
+
+	}
+
+}
---
+++ Cone_HourGlass/Cone_HourGlass/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Cone_HourGlass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+            bool check = true;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("1: Draw a cone");
+                Console.WriteLine("2: Draw an Hourglass");
+                Console.WriteLine("3: Exit");
+                Console.WriteLine("");
+
+                try
+                {
+                  choice = int.Parse(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            do
+                            {
+                                try
+                                {
+                                    Console.WriteLine("Enter a number to create a cone");
+                                    int cone_number = int.Parse(Console.ReadLine());
+                                    Art cone_art = new Art();
+                                    cone_art.ConeArt(cone_number);
+                                    check = true;
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("Invalid Format- try again");
+                                    check = false;
+                                }
+                            } while (check == false);
+                            break;
+
+                        case 2:
+                            do
+                            {
+                                try
+                                {
+                                    Console.WriteLine("Enter a number to create an hourglass");
+                                    int hourglass_number = int.Parse(Console.ReadLine());
+                                    Art hourglass_art = new Art();
+                                    hourglass_art.HourglassArt(hourglass_number);
+                                    check = true;
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine("Invalid Format- try again");
+                                    check = false;
+                                }
+                            } while (check == false);
+
+                            break;
+                        case 3:
+                            Console.WriteLine("Exiting.....BYE!");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid choice.....Enter a number from the menu");
+                            break;
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine("Invalid Format");
+                }
+
+            } while (choice != 3);
+           
+
+            
+
+        }
+    }
+}
